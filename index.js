db0d51e82cb3ebfcaba1b2291415a1bde5a6df75
venkatsp431 <sealvenki@gmail.com>
2024-02-28 16:27:39 +0530

    Inital Commit

---
+++ index.js
@@ -0,0 +1,61 @@
+import express from "express";
+import bodyParser from "body-parser";
+import nodemailer from "nodemailer";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import cors from "cors";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+// Serve static files (optional, depending on your project structure)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Handle form submissions
+app.post("/api/contact", (req, res) => {
+  // Extract form data from the request body
+  const { name, email, subject, message } = req.body;
+
+  // Do something with the form data (e.g., send an email, store in a database)
+
+  // Send email
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "sealvenki@gmail.com", // your email
+      pass: "hpuv xape ilsd bwgr", // your email password
+    },
+  });
+
+  const mailOptions = {
+    // from: "sealvenki@gmail.com",
+    to: "sealvenki@gmail.com", // recipient's email
+    subject: `New Contact Form Submission - ${subject}`,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Email error:", error);
+      res
+        .status(500)
+        .json({ success: false, message: "Failed to send email." });
+    } else {
+      console.log("Email sent:", info.response);
+      res.json({ success: true, message: "Message received successfully!" });
+    }
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
